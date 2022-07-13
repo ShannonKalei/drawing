a4acf456afa6d1fb8e22fd6408e53b82320afd4d
ShannonKalei <shannonkalei@gmail.com>
2022-07-12 22:59:03 -0700

    Add basic color theming

---
+++ src/components/ColorThemeSlider.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import createPersistedState from 'use-persisted-state';
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+const useColorSchemeState = createPersistedState('colorScheme');
+
+
+export default function ColorThemeSlider() {
+  const systemPrefersDark = useMediaQuery({query: '(prefers-color-scheme: dark)'});
+  const [ prevTheme, setPrevTheme ] = useState(systemPrefersDark ? 'dark' : 'light');
+  const [ activeTheme, setActiveTheme ] = useColorSchemeState();
+
+  const colorThemes = {
+    1: "sunrise",
+    2: "light",
+    3: "sunset",
+    4: "night",
+    5: "dark"
+  }
+
+  useEffect(() => {
+    document.body.classList.add(activeTheme);
+    if (activeTheme !== prevTheme) {
+      document.body.classList.remove(prevTheme);
+    }
+
+  }, [activeTheme]);
+
+  function getColorThemeKey(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+  
+ const handleColorThemeChange = (theme) => {
+  setActiveTheme(colorThemes[theme]);
+ }
+
+  return (
+    <React.Fragment>
+      <Slider
+          className='color-theme-slider'
+          value={getColorThemeKey(colorThemes, activeTheme)}
+          min={1}
+          max={5}
+          step={1}
+          onChange={(theme) => {
+            setPrevTheme(activeTheme);
+            handleColorThemeChange(theme);
+          }}
+          marks={colorThemes}
+          included={false}
+        />
+    </React.Fragment>
+      
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import DrawingScene from "./components/DrawingScene";
+import ColorThemeSlider from "./components/ColorThemeSlider"
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ColorThemeSlider />
     <DrawingScene />
   </React.StrictMode>
 );
