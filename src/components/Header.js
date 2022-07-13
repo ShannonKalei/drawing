319211c0b11c97303d6a2ea5819ae2008a71fc13
ShannonKalei <shannonkalei@gmail.com>
2022-07-13 16:43:04 -0700

    Update header styling and responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import ColorThemeSlider from "./ColorThemeSlider";
 
 export default function Header() {
   return (
-    <div className="flex-container space-between">
+    <div className="flex-container space-between header">
       <h1>Just a drawing app</h1>
       <ColorThemeSlider/>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import DrawingScene from "./components/DrawingScene";
-import ColorThemeSlider from "./components/ColorThemeSlider"
 import Header from "./components/Header";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="page-container">
-      <Header />
-      <DrawingScene />
+    <div className="full-page">
+      <div className="page-container">
+        <Header />
+        <DrawingScene />
+      </div>
     </div>
   </React.StrictMode>
 );
